df32413ac1cabe9501ee3741863a7771ffe2b7ed
MANAS SRI CHARAN Varri <manassricharanvarri@MANASs-MacBook-Pro.local>
2022-12-22 12:47:33 +0530

    added funcitonality to selecr all the images in a given folder

--- image_watermark/main.py
+++ image_watermark/main.py
@@ -1,5 +1,7 @@
 from PIL import Image
+
 import os
+import re
 
 def blender(img_path,watermark_path):
 	img = Image.open(img_path)
@@ -11,11 +13,17 @@ def blender(img_path,watermark_path):
 	img.paste(w_img,(img_size[0]-w_img.size[0]-20,img_size[1]-w_img.size[1]-	20),w_img)
 	return img
 if __name__=="__main__":
-	img_path = input("enter the image path")
+	folder_path = input("enter the image path")
 	watermark_path = input("enter the watermark path")
+	
+	os.chdir(folder_path)	
+	images = [element for element in os.listdir() if re.match(r'(.*\.png)|(.*\.jpg)',element)]
+	print(images)
+"""
 	final_img = blender(img_path,watermark_path)
 	#conver the final image to rgb as jpeg doesnt support rgba
 	final_img = final_img.convert("RGB")
 	print("saving the final image ")
 	final_img.save("output.jpg")
 	final_img.show()
+"""
