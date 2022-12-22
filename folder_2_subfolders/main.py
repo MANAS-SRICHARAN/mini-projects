49418bb620926e48c3f1dee2ca7ca8ce89d1af28
MANAS SRI CHARAN Varri <manassricharanvarri@MANASs-MacBook-Pro.local>
2022-12-22 16:19:13 +0530

    completed funcitonality to chop th elements individually

--- folder_2_subfolders/main.py
+++ folder_2_subfolders/main.py
@@ -4,9 +4,34 @@ import shutil
 import sys
 
 
+def file_chopper(elements,count):
+	total_elements = len(elements)
+	print(total_elements,"te")
+	for i in range(1,total_elements+1,count):
+		diff = total_elements -i+1
+		if i+count <= total_elements:  # to prevent index overflow
+			start, end = i,i+count
+		else:
+			if diff >0:
+				start,end = i, i+diff
+		yield elements[start-1:end-1]
+
+def splitter(source,count):
+	#get the list of all elements in the directory
+	os.chdir(source)
+	elements = os.listdir()
+	chopped_elements =file_chopper(elements,count)
+	for i,j in enumerate(chopped_elements):
+		print(i,j)
+
+
 if __name__=="__main__":
 	if len(sys.argv)<3:
 		print("enter proper parameters required")
 	else:
-		print(sys.argv[1],"**",sys.argv[2])
+		source,count = sys.argv[1],int(sys.argv[2])
+		if os.path.isdir(source):
+			splitter(source,count)
+		else:
+			sys.exit(0)
 
