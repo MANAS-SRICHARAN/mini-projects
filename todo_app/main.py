8ef72b9a62e554589d2e84fa0e6449f1d296ab3f
MANAS SRI CHARAN Varri <manassricharanvarri@MANASs-MacBook-Pro.local>
2022-12-26 10:28:49 +0530

    smash update issue reslved

--- todo_app/main.py
+++ todo_app/main.py
@@ -52,9 +52,17 @@ def update():
 		if len(list(request.form.keys())) == 1:
 			tasks=Todo.query.order_by(Todo.date).all()
 			return render_template('index.html',tasks=tasks)
-		update_ids = request.form
-		return update_ids
-		
+		else:
+			update_ids = request.form
+			update_ids=[int(val) for val in update_ids.values() if val !="refresh"]
+			
+			db.session.execute(db.update(Todo).where(Todo.id.in_(update_ids)).values(completion=1))
+			db.session.commit()
+			tasks=Todo.query.order_by(Todo.date).all()
+			return render_template('index.html',tasks=tasks)
+
+
+	
 if __name__=="__main__":
 	with app.app_context():	
 		db.create_all()
