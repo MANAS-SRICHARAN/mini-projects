6f1a70f83a610f207f7ebd256ef82973b77bc2a5
MANAS SRI CHARAN Varri <manassricharanvarri@MANASs-MacBook-Pro.local>
2022-12-26 12:18:54 +0530

    added functionality to show % and time left for battery

---
+++ battery_notification/main.py
@@ -0,0 +1,18 @@
+import psutil
+
+def secs_formatter(time):
+	min,sec = divmod(time,60)
+	hr,min = divmod(min,60)
+	return f"{hr}:{min}:{sec} left"
+
+def sensor_formatter(sensor):
+	percent = sensor.percent
+	if sensor.power_plugged:
+		time = "infinity as it is plugged in"
+	else:
+		time = sensor.secsleft
+		time = secs_formatter(time)
+	return [percent,time]
+percent,time =sensor_formatter(psutil.sensors_battery()) 
+print(f"% is {percent}")
+print(f"{time}") 
